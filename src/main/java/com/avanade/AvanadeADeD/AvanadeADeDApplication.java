9fb82c2480068e24ecdb6274b97b4e6db91d2602
Fulvio Manfrin <manfrin.fulvio@gmail.com>
2023-09-10 21:04:00 -0300

    Initial Commit

---
+++ src/main/java/com/avanade/AvanadeADeD/AvanadeADeDApplication.java
@@ -0,0 +1,13 @@
+package com.avanade.AvanadeADeD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeADeDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeADeDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avanade/AvanadeADeD/AvanadeADeDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanade.AvanadeADeD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeADeDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
